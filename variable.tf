eac696cb3700cad0f7b97fc670ef8097e7cf27a3
Akhil Khattar <khattar.akhil693@gmail.com>
2025-01-22 05:08:58 -0500

    added var file

--- variable.tf
+++ variable.tf
@@ -1,2 +1,32 @@
 variable "region" {
 }
+variable "vpc_name" {
+    default = whizlabs_VPC
+}
+variable "IGW" {
+  default = IGW
+}
+
+variable "Public-Subnet" {
+  default = Public-Subnet
+}
+
+variable "Private-Subnet" {
+  default = Private-Subnet
+}
+
+variable "natgw" {
+  default = natgw
+}
+
+variable "WebSG" {
+  default = WebSG
+}
+
+variable "PrivateServer" {
+  default = PrivateServer
+}
+
+variable "PublicWebServer" {
+  default = PublicWebServer
+}
